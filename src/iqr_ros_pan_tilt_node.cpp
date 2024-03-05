2dab3e0ac55362d121f2a4829adf4324cd6d9911
Josh Maximoff <jmaximoff@numurus.com>
2024-03-05 13:40:11 -0500

    License updates - universal BSD-3-Clause for nepi-engine

--- src/iqr_ros_pan_tilt_node.cpp
+++ src/iqr_ros_pan_tilt_node.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024 Numurus, LLC <https://www.numurus.com>.
+ *
+ * This file is part of nepi-engine
+ * (see https://github.com/nepi-engine).
+ *
+ * License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+ */
 #include "iqr_ros_pan_tilt_node.h"
 #include "ptx_interface.h"
 #include "drivers/iqr_ros_pan_tilt/PanTiltDriver.h"
